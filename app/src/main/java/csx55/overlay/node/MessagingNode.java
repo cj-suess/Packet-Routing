1b7f57fb3f6b9bc02edde0cf69b8ddd47b132fc8
Cameron Suess <camsuess@grouper.cs.colostate.edu>
2025-09-01 10:43:21 -0600

    added deregistration and tested with a single successful deregistration. need to test failures for both registration and deregistration next

--- app/src/main/java/csx55/overlay/node/MessagingNode.java
+++ app/src/main/java/csx55/overlay/node/MessagingNode.java
@@ -12,21 +12,33 @@ public class MessagingNode implements Node {
 
     String hostname;
     int port;
+    boolean registered;
 
     ServerSocket serverSocket;
     static int serverPort;
-    static Socket socket;
+
+    // Registry info
+    static Socket registrySocket;
+    TCPSender registrySender;
+    TCPReceiverThread registryReceiver;
 
 
     public MessagingNode(String hostname, int port) {
         this.hostname = hostname;
         this.port = port;
+        this.registered = false;
     }
 
     public void onEvent(Event event, TCPSender sender) {
         if(event.getType() == Protocol.REGISTER_RESPONSE) {
             Message message = (Message) event; // downcast back to Message
             System.out.println(message.info);
+            if(message.statusCode == 0) { registered = true; }
+        }
+        if(event.getType() == Protocol.DEREGISTER_RESPONSE) {
+            Message message = (Message) event; // downcast back to Message
+            System.out.println(message.info);
+            if(message.statusCode == 0) { registered = false; }
         }
     }
 
@@ -35,6 +47,16 @@ public class MessagingNode implements Node {
             serverSocket = new ServerSocket(0);
             serverPort = serverSocket.getLocalPort();
             System.out.println("[MessagingNode] Messaging node is up and running.\n \t[MessagingNode] Listening on port: " + serverPort + "\n" + "\t[MessagingNode] IP Address: " + serverSocket.getInetAddress().getHostAddress());
+            register();
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> { // needed if the terminal crashes so the node deregisters. not sure if I can catch it elsewhere
+                try {
+                    if(registered) { deregister(); }
+                    serverSocket.close();
+                } catch(IOException e) {
+                    System.err.println("Exception while trying to clean up after sudden termination...");
+                }
+            }));
 
             while(true) {
                 Socket socket = serverSocket.accept();
@@ -42,6 +64,7 @@ public class MessagingNode implements Node {
                 TCPServerThread st = new TCPServerThread(socket, this);
                 new Thread(st).start();
             }
+
         } catch(IOException e) {
             System.out.println("[MessagingNode] Exception while starting messaging node..." + e.getMessage());
         }
@@ -55,9 +78,12 @@ public class MessagingNode implements Node {
                 switch (command) {
                     case "exit":
                         System.out.println("[MessagingNode] Closing messaging node...");
+                        if(registered == true) { deregister(); }
                         System.exit(0);
                         scanner.close();
                         break;
+                    case "deregister":
+                        deregister();
                     default:
                         break;
                 }
@@ -69,18 +95,35 @@ public class MessagingNode implements Node {
 
     public void register() {
         try {
-            socket = new Socket(hostname, port);
-            System.out.println("[MessagingNode] Connecting to registry...\n" + "[MessagingNode] Local Port: " + socket.getLocalPort() +"\n" + "[MessagingNode] Remote Port: " + port);
+            registrySocket = new Socket(hostname, port);
+            System.out.println("[MessagingNode] Connecting to registry...\n" + "[MessagingNode] Local Port: " + registrySocket.getLocalPort() +"\n" + "[MessagingNode] Remote Port: " + port);
             // create register request instance
-            Register registerRequest = new Register(Protocol.REGISTER_REQUEST, socket.getLocalAddress().getHostAddress(), serverPort);
+            Register registerRequest = new Register(Protocol.REGISTER_REQUEST, registrySocket.getLocalAddress().getHostAddress(), serverPort);
             // create sender instance to send register request
-            TCPSender sender = new TCPSender(socket);
+            registrySender = new TCPSender(registrySocket);
             // create receiving thread for response?
-            TCPReceiverThread receiver = new TCPReceiverThread(socket, this, sender);
-            new Thread(receiver).start();
-            sender.sendData(registerRequest.getBytes());
+            registryReceiver = new TCPReceiverThread(registrySocket, this, registrySender);
+            new Thread(registryReceiver).start();
+            registrySender.sendData(registerRequest.getBytes());
         } catch (IOException e) {
             System.out.println("[MessagingNode] Exception while registering node with registry...");
+        }
+    }
+
+    public void deregister() {
+        if(registrySender == null) {
+            System.out.println("No connection exists with the registry...");
+        }
+        else if(registered == false) {
+            System.out.println("This node is not registered. No deregistration request can be sent...");
+        } else {
+            System.out.println("[MessagingNode] Deregistering node...");
+            Deregister deregisterRequest = new Deregister(Protocol.DEREGISTER_REQUEST, registrySocket.getLocalAddress().getHostAddress(), serverPort);
+            try {
+                registrySender.sendData(deregisterRequest.getBytes());
+            } catch (IOException e) {
+                System.err.println("Exception while deregitering node..." + e.getMessage());
+            }
         }
     }
 
@@ -88,7 +131,6 @@ public class MessagingNode implements Node {
         MessagingNode node = new MessagingNode(args[0], Integer.parseInt(args[1]));
         new Thread(node::startNode).start();
         new Thread(node::readTerminal).start();
-        node.register();
     }
     
 }
